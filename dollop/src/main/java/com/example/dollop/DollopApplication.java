454957a177d7306335238f59e7d8f2f5f5743965
Maxence DESBOUYS <madesbouys@IUTCLINFA2319.iut.local>
2022-05-12 13:58:31 +0200

    initial d

---
+++ dollop/src/main/java/com/example/dollop/DollopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dollop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollopApplication.class, args);
+	}
+
+}
---
+++ dollop/src/test/java/com/example/dollop/DollopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dollop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7376aa897913a3fd7a0d4b20732c9ea7bd4d4514
madesbouys <maxence.desbouys@etu.uca.fr>
2022-06-09 15:02:47 +0200

    javaDoc serviceIMPL

--- dollop/src/main/java/com/example/dollop/service/impl/UserServiceImpl.java
+++ dollop/src/main/java/com/example/dollop/service/impl/UserServiceImpl.java
@@ -25,32 +25,80 @@ public class UserServiceImpl implements UserService {
         UserServiceImpl.repoStatic = repo;
     }
 
+    /**
+     * méthode permettant d'obtenir tous les utilisateurs en base  
+     * @author Guillaume Valentin
+     * @see UserDto
+     * @return la liste de tous les utilisateurs en base 
+     * @since 1.0
+     */
     @Override
     public List<UserDto> findAll() {
         return repo.findAll()
             .stream().map(c -> new UserDto(c)).collect(Collectors.toList());
     }
 
+    /**
+     * méthode permettant d'obtenir un utilisateur par son identifiant  
+     * @author Guillaume Valentin
+     * @see UserDto
+     * @param id identifiant de l'utilistaeur en BDD au format chaine de caractère
+     * @return l'utilisateur recherché
+     * @throws Exception utilisateur non trouvé
+     * @since 1.0
+     */
     @Override
     public UserDto findById(String id) {
         return new UserDto(repo.findUserById(id));
     }
 
+    /**
+     * méthode static permettant d'obtenir un utilisateur par son identifiant 
+     * @author Guillaume Valentin
+     * @see UserDto
+     * @param id identifiant de l'utilistaeur en BDD au format chaine de caractère
+     * @return l'utilisateur recherché
+     * @throws Exception utilisateur non trouvé
+     * @since 1.0
+     */
     public static UserDto findByIdStatic(String id) {
         return new UserDto(repoStatic.findUserById(id));
     }
 
+    /**
+     * méthode permettant de supprimer un utilisateur par son id 
+     * @author Guillaume Valentin
+     * @see UserDto
+     * @param id identifiant de l'utilistaeur en BDD au format chaine de caractère
+     * @throws Exception utilisateur non trouvé
+     * @since 1.0
+     */
     @Override
     public void deleteById(String id) {
         repo.deleteById(new ObjectId(id));
     }
 
+    /**
+     * méthode permettant de sauvegarder un utilisateur par son id 
+     * @author Guillaume Valentin
+     * @see UserDto
+     * @param user l'utilisateur a sauvegardé en base
+     * @since 1.0
+     */
     @Override
     public void save(UserDto user) {
         repo.save(user.toModel());
         
     }
 
+    /**
+     * méthode de recherche d'un utilisateur par son nom
+     * @author Guillaume Valentin
+     * @see UserDto
+     * @param name recherche l'utilisteur par son nom
+     * @throws Exception utilisateur non trouvé
+     * @since 1.0
+     */
     @Override
     public UserDto findByName(String name) {
         return new UserDto(repo.findUserByName(name));
